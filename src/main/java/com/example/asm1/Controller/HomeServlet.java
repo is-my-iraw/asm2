c2d2b4995a25034d21684844fa24328d1ce584e0
assignment <quanpmth2009008@fpt.edu.vn>
2022-05-30 14:10:20 +0700

    base2

--- src/main/java/com/example/asm1/Controller/HomeServlet.java
+++ src/main/java/com/example/asm1/Controller/HomeServlet.java
@@ -7,32 +7,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
+
 
 @WebServlet(name = "HomeServlet", value = "/home")
 public class HomeServlet extends HttpServlet {
     ProductDao dao = new ProductDao();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String action = request.getServletPath();
-//        switch (action){
-//            case "/new":
-//                service(request, response);
-//                break;
-//            case "/insert":
-//                doPut(request, response);
-//                break;
-//            case "/delete":
-//                doDelete(request,response);
-//                break;
-//            case "/update":
-//                update(request, response);
-//                break;
-//            default:
-//                break;
-//        }
-//        request.setAttribute("products", productList());
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
@@ -46,25 +27,4 @@ public class HomeServlet extends HttpServlet {
         dao.insertUser(products);
         resp.sendRedirect("list");
     }
-//    protected List<Products> productList(){
-//        List<Products> list = dao.getListProduct();
-//        return list;
-//    }
-//
-//    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        int id = Integer.parseInt(req.getParameter("id"));
-//        dao.DeleteProduct(id);
-//        resp.sendRedirect("list");
-//    }
-//
-//    private void update(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException{
-////        int id  = Integer.parseInt(request.getParameter("id"));
-//        String name = request.getParameter("name");
-//        String email = request.getParameter("email");
-//        String address = request.getParameter("address");
-//        String phone = request.getParameter("phone");
-//        Products products = new Products(name, email, address, Integer.valueOf(phone));
-//        dao.UpdateProduct(products);
-//        response.sendRedirect("list");
-//    }
 }
